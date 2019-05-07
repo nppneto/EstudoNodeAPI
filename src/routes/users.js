6a3b3d85326ec07d040798a3792b4e68a25c7616
Nelson Neto <nelsonp2n@gmail.com>
2019-05-07 11:20:15 -0300

    Corrigindo gitignore

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/userController');
 
+// Rotas
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/:id', controller.delete);
