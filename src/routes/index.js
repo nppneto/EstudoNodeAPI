5399baf1eac47d196a1691c88e402bac1cc35d5b
Nelson Neto <nelsonp2n@gmail.com>
2019-05-07 10:44:14 -0300

    Início estrutura node api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//Rotas
 router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Express API",
